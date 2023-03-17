ea16197f5c8512f0fd5041755e63041b470f7264
Carmelo Rubiolo <melo_rubiolo@hotmail.com>
2023-03-17 19:54:55 -0300

    pagination2

--- src/components/Quads/Quads.js
+++ src/components/Quads/Quads.js
@@ -11,15 +11,24 @@ const Quads = () => {
     
     const Quads = exercises.filter(p => p.target === "quads");
 
+    const [ currentPage, setCurrentPage ] = useState(0)
+    const nextPage = () => {
+        setCurrentPage( currentPage + 10)
+    }
+    const prevPage = () => {
+        if( currentPage > 0)
+        setCurrentPage( currentPage - 10)
+    }
+
     return (
         <>
         <div className='back-container'>
-            <Link to="/"><button>Volver</button></Link>
+        <Link to="/"><button  id="inicio">Volver</button></Link>
         </div>
         <div className='container'>
             <div className='row'>
 
-            {exercises != null ? (Quads.map(ej => (
+            {exercises != null ? (Quads.slice(currentPage, currentPage + 10).map(ej => (
                 <div className="col" key={ej.id}>
                     <div className='card'>
                         <h4 className="card-title">{ej.name}</h4>
@@ -33,6 +42,20 @@ const Quads = () => {
             ))) : <div>no hay ejercicios</div>}
             </div>
         </div>
+        <footer className='footer-page'>
+            <a href="#inicio"><button 
+            className='btn'
+            onClick={prevPage}
+            >
+                Back
+            </button></a>
+            <a className="go-up" href="#inicio"><button 
+            className='btn'
+            onClick={nextPage}
+            >
+                Next
+            </button></a>
+        </footer>
         </>
     )
 }
